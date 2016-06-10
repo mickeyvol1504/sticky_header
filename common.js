639519a2a19282e6a09a74ad8e83ce476a77cea5
Michael Voloshyn <michael.voloshyn@gmail.com>
2016-06-10 07:51:05 +0300

    Add condition for touch screen

--- common.js
+++ common.js
@@ -1,16 +1,30 @@
 function sticky(el) {
 	var tagTop = el.getBoundingClientRect().top;
 	var onscroll;
-	if (window.onscroll)
-		onscroll = window.onscroll;
-	window.onscroll = function(event) {
+
+	if ('touchmove' in window) {
+		onscroll = window.touchmove;
+		document.addEventListener('touchmove', function(event){
+			event.preventDefault();
+			var pageY = window.pageYOffset || document.documentElement.scrollTop;
+			if (pageY > tagTop)
+				el.className="sticky fixed";
+			else
+				el.className="sticky";
+			onscroll && onscroll(event);
+		}, false);
+	} else {
+		if (window.onscroll)
+			onscroll = window.onscroll;
+		window.onscroll = function(event) {
 		var pageY = window.pageYOffset || document.documentElement.scrollTop;
 		if (pageY > tagTop)
 			el.className="sticky fixed";
 		else
 			el.className="sticky";
 		onscroll && onscroll(event);
-	};
+		};
+	}
 };
 
 var arraySticky = document.querySelectorAll('.sticky');
